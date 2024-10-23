918c8da8d913d2b9af7b66048ea922bb208365ae
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-10-23 16:50:37 +0200

    setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,32 +1,17 @@
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import { Text } from "react-native";
-import LoginScreen from "../components/LoginScreen";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Slot, Stack} from 'expo-router'
 
-export default function RootLayout() {
-  useFonts({
-    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
-    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
-    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
-  });
+const RootLayout = () => {
   return (
-    <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
-      <SignedIn>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-      </SignedIn>
-
-      <SignedOut>
-        <LoginScreen />
-      </SignedOut>
-    </ClerkProvider>
-  );
+    <>
+   <Stack>
+    <Stack.Screen name="index"
+    options={{headerShown: false}}/>
+   </Stack>
+    </>
+  )
 }
+
+export default RootLayout
+
--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,15 @@
-import { Redirect } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
-export default function Index() {
+export default function App() {
   return (
-    <Redirect href={'/home'}/>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Prime App</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
   );
 }
